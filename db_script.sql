7f72f35e56675f9dbfe2c186aa308f3c164de8f2
lucibrezaiu <lucian.brezaiu@student.unitbv.ro>
2020-06-28 17:38:06 +0300

    db script updated

--- db_script.sql
+++ db_script.sql
@@ -63,6 +63,24 @@ CREATE TABLE `identityroleresource` (
   CONSTRAINT `FK_IDENTITYROLERESOURCES_ROLE` FOREIGN KEY (`roleId`) REFERENCES `role` (`roleId`) ON DELETE NO ACTION ON UPDATE NO ACTION
 )
 
+//seed
+
+SET SQL_SAFE_UPDATES = 0;
+delete from managementsystem.identityroleresource;
+ALTER TABLE managementsystem.identityroleresource AUTO_INCREMENT = 1;
+delete from managementsystem.roleright;
+ALTER TABLE managementsystem.roleright AUTO_INCREMENT = 1;
+delete from managementsystem.right;
+ALTER TABLE managementsystem.right AUTO_INCREMENT = 1;
+delete from managementsystem.role;
+ALTER TABLE managementsystem.role AUTO_INCREMENT = 1;
+delete from managementsystem.resource;
+ALTER TABLE managementsystem.resource AUTO_INCREMENT = 1;
+delete from managementsystem.identity;
+ALTER TABLE managementsystem.identity AUTO_INCREMENT = 1;
+delete from managementsystem.organization;
+ALTER TABLE managementsystem.organization AUTO_INCREMENT = 1;
+
 insert into managementsystem.organization(organizationName,cui) values 
 ('IBM','34318904'),
 ('Atos','98066608'),
@@ -74,25 +92,43 @@ insert into managementsystem.organization(organizationName,cui) values
 
 insert into managementsystem.identity(organizationId,username,email,password,firstName,lastName) values
 (1,'admin','admin@outlook.com','admin','admin','admin'),
-(7,'lucianbrezaiu','lucianbrezaiu@yahoo.com','Copernic@1234','Lucian','Brezaiu');
+(7,'lucianbrezaiu#123','lucianbrezaiu@yahoo.com','Copernic@1234','Lucian','Brezaiu'),
+(5,'ion.mihnea#475','ion.mihnea@gmail.com','Copernic@1234','Alexandru-Mihnea','Ion'),
+(7,'an.vieru#792','an.vieru@gmail.com','Copernic@1234','Andreea','Vieru'),
+(7,'mihai#389','mihai@gmail.com','Copernic@1234','Mihai','Vilcu');
 
 insert into managementsystem.role(roleName,roleDescription) values
 ('idp_admin','administrator of identity mangement system'),
-('idp_member','member of identity mangement system');
+('idp_member','member of identity mangement system'),
+('intranet_admin','administrator of resource intranet'),
+('intranet_member','member of resource intranet');
 
 insert into managementsystem.right(rightName,rightDescription) values
 ('create_organisations','create organisations in identity system'),
 ('create_resources','create resources in identity system'),
 ('create_roles','create roles in identity system'),
-('create_rights','create rights in identity system');
+('create_rights','create rights in identity system'),
+('see_profile','visualize profile in identity system'),
+('see_claims','visualize user claims in identity system');
+
+insert into roleright(roleId,rightId) values
+(1,1),
+(1,2),
+(1,3),
+(1,4),
+(2,5),
+(2,6);
 
 insert into managementsystem.resource(resourceName) values 
 ('Dox'),
 ('Intranet'),
-('Condica Online'),
+('Condica'),
 ('Identity Management System');
 
 insert into managementsystem.identityroleresource(identityId,roleId,resourceId) values 
 (1,1,4),
-(2,2,4);
-
+(1,2,4),
+(2,2,4),
+(3,2,4),
+(4,2,4),
+(5,2,4);
